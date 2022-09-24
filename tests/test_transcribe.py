447d2f612fa0900fe45065ad0b494c89e37fb502
fcakyon <fcakyon@gmail.com>
2022-09-25 01:25:47 +0300

    remove ffmpeg cli dependency

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -7,7 +7,7 @@ import pywhisper
 
 @pytest.mark.parametrize('model_name', pywhisper.available_models())
 def test_transcribe(model_name: str):
-    model = pywhisper.load_model(model_name).cuda()
+    model = pywhisper.load_model(model_name)
     audio_path = os.path.join(os.path.dirname(__file__), "jfk.flac")
 
     language = "en" if model_name.endswith(".en") else None
